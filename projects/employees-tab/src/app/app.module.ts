f870c4abfa6f9b87b1094519b5dde7d24fa99fe2
Juanse Franco <jsfc2199@gmail.com>
2024-07-31 20:36:22 -0500

    se eliminan detalles de la card para enviarlar con click a otro micro front

--- projects/employees-tab/src/app/app.module.ts
+++ projects/employees-tab/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeCardComponent } from './employee/employee-card/employee-card.component';
-import { EmployeeTableComponent } from './employee/employee-table/employee-table.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+
   ],
   imports: [
     BrowserModule,
